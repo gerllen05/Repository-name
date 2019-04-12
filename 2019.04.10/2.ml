d406aec7eed899b5c64652fd242ca8493cd1605c
gerllen05 <gerllen05@gmail.com>
2019-04-12 14:32:23 +0300

    2

--- 2019.04.10/2.ml
+++ 2019.04.10/2.ml
@@ -2,7 +2,7 @@ type tree = Node of int*tree*tree | Leaf;;
 let a = Node(1,Node(2,Leaf,Node(3,Leaf,Leaf)),Node(4,Leaf,Leaf));;
 let rec glub a = match a with
 	Leaf -> 0
-	|Node(num,t1,t2) -> 1 + (max (glub t1) (glub t2));;
+	|Node(num,t1,t2) -> 1 + (glub t1) + (glub t2);;
 let rec write a m n = match a with
 	Leaf -> ()
 	|Node(num,t1,t2) -> if m <> n then ((write t1 (m+1) n);(write t2 (m+1) n)) else (print_int num;(write t1 (m+1) n);(write t2 (m+1) n));;

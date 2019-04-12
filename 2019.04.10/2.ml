017d782a8bb26ff6ac833292467b21558066e8bd
gerllen05 <gerllen05@gmail.com>
2019-04-12 14:58:41 +0300

    2

--- 2019.04.10/2.ml
+++ 2019.04.10/2.ml
@@ -5,7 +5,7 @@ let rec glub a = match a with
 	|Node(num,t1,t2) -> 1 + (glub t1) + (glub t2);;
 let rec write a m n = match a with
 	Leaf -> ()
-	|Node(num,t1,t2) -> if m <> n then ((write t1 (m+1) n);(write t2 (m+1) n)) else (print_int num;(write t1 (m+1) n);(write t2 (m+1) n));;
+	|Node(num,t1,t2) -> if m <> n then (write t1 (m+1) n;write t2 (m+1) n) else (print_int num;write t1 (m+1) n;write t2 (m+1) n);;
 for i = 0 to (glub a) do (write a 0 i) done;; 
 
 	

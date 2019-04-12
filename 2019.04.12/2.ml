ca54dc5ab624594fd2c76bfaa949dddaa516e641
gerllen05 <gerllen05@gmail.com>
2019-04-12 15:51:07 +0300

    123

--- 2019.04.12/2.ml
+++ 2019.04.12/2.ml
@@ -2,6 +2,6 @@ type tree = Node of int*tree*tree | Leaf;;
 let a = Node(2,Node(1,Leaf,Leaf),Node(3,Leaf,Leaf));;
 let x = read_int();;
 let rec find a x = match a with
-	Leaf -> "False"
-	|Node(num,t1,t2) -> if x = num then "True" else (if x < num then find t1 x else find t2 x);;
-print_string (find a x);;
+	Leaf -> false
+	|Node(num,t1,t2) -> if x = num then true else (if x < num then find t1 x else find t2 x);;
+Printf.printf "%b"(find a x);;
--- 2019.04.12/3.ml
+++ 2019.04.12/3.ml
@@ -1,5 +1,9 @@
 type tree = Node of int*tree*tree | Leaf;;
 let a = Node(2,Node(1,Leaf,Leaf),Node(3,Leaf,Leaf));;
+let x = read_int();;
+let rec fill a x = match a with
+	Leaf -> Node(x,Leaf,Leaf)
+	|Node(num,t1,t2) -> if x < num then Node(num,(fill t1 x),t2) else Node(num,t1,(fill t2 x));;
 let rec lkp a = match a with
 	Leaf -> []
 	|Node(num,t1,t2) -> (lkp t1) @ [num] @ (lkp t2);;
@@ -7,4 +11,5 @@ let rec upor l = match l with
 	[] -> true
 	|a::[] -> true
 	|a::b::c -> if a > b then false else (upor (b::c));;
-Printf.printf "%b" (upor (lkp a));;
+Printf.printf "%b"(upor (lkp (fill a x)));;
+	

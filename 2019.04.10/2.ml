337c8a5c073da3e4cc9fae05a36f0febc653570e
gerllen05 <gerllen05@gmail.com>
2019-04-12 14:32:55 +0300

    2

--- 2019.04.10/2.ml
+++ 2019.04.10/2.ml
@@ -6,6 +6,6 @@ let rec glub a = match a with
 let rec write a m n = match a with
 	Leaf -> ()
 	|Node(num,t1,t2) -> if m <> n then ((write t1 (m+1) n);(write t2 (m+1) n)) else (print_int num;(write t1 (m+1) n);(write t2 (m+1) n));;
-for i = 0 to (glub a) do (write a 0 i) done;;
+for i = 0 to (glub a) do (write a 0 i) done;; 
 
 	

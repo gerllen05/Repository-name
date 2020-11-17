54441f04c8c4f39cc523b5c6cfdd98f89449ad2e
gerllen05 <49240493+gerllen05@users.noreply.github.com>
2020-11-18 00:43:20 +0300

    Update 2.ml

--- 2020.11.16/2.ml
+++ 2020.11.16/2.ml
@@ -2,13 +2,13 @@ type tree = Leaf| Node of tree*tree*int;;
 
 let t = Node(Node(Leaf,Leaf,0),Node(Leaf,Leaf,3),2);;
 
-let rec add t x lr = match t with
+let rec add t x = match t with
 	Leaf -> Node(Leaf,Leaf,x)
-	|Node(a,b,c) -> if lr = 0 then (if x<c then Node(add a x 0,b,c) else Node(add a c 0,b,x)) else (if x<c then Node(a,add b x 1,c) else Node(a,add b c 1,x));;
+	|Node(Leaf,Leaf,e) -> if x<e then Node(Node(Leaf,Leaf,x),Leaf,e) else Node(Leaf,Node(Leaf,Leaf,x),e)
+	|Node(a,b,e) -> if x<e then Node(add a x,b,e) else Node(a,add b x,e);;
 	
-
 let rec string_of_tree t = match t with
 	Leaf -> "*"
 	|Node(a,b,c) -> "(" ^ (string_of_tree a) ^ "," ^ (string_of_tree b) ^ "," ^ (string_of_int c) ^ ")";;
 	
-print_string (string_of_tree (add t 1 0));;
+print_string (string_of_tree (add t 1));;

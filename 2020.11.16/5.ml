530613f05bd777f260d1eeec0874c425f24ae28d
gerllen05 <49240493+gerllen05@users.noreply.github.com>
2020-11-18 00:57:10 +0300

    Update 5.ml

--- 2020.11.16/5.ml
+++ 2020.11.16/5.ml
@@ -1,1 +1,18 @@
+type tree = Leaf| Node of tree*tree*int;;
 
+let t = Node(Node(Leaf,Leaf,0),Node(Node(Leaf,Leaf,1),Leaf,3),2);;
+
+let rec ver t = match t with
+	Leaf -> true
+	|Node(Leaf,Leaf,e) -> true
+	|Node(Node(a,b,c),Leaf,e) -> if c<e then ver (Node(a,b,c)) else false
+	|Node(Leaf,Node(r,p,q),e) -> if q>e then ver (Node(r,p,q)) else false
+	|Node(Node(a,b,c),Node(r,p,q),e) -> 
+		if c<e then 
+			(if q>e then 
+				((ver (Node(a,b,c))) && (ver (Node(r,p,q))))
+			else false)
+		else false;;
+		
+Printf.printf "%b" (ver t);;
+	

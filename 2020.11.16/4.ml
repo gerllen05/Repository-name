ca44a5ce04a0e6fe27b4110e1498e87e7637cac7
gerllen05 <49240493+gerllen05@users.noreply.github.com>
2020-11-18 10:41:08 +0300

    Update 4.ml

--- 2020.11.16/4.ml
+++ 2020.11.16/4.ml
@@ -1,9 +1,18 @@
 type tree = Leaf| Node of tree*tree*int;;
 
-let t = Node(Node(Leaf,Leaf,1),Node(Leaf,Leaf,3),2);;
+let t = Node(Node(Leaf,Leaf,1),Node(Leaf,Node(Leaf,Leaf,4),3),2);;
 
-let rec string_of_tree t = match t with
-	Leaf -> "*"
-	|Node(a,b,c) -> "(" ^ (string_of_tree a) ^ "," ^ (string_of_tree b) ^ "," ^ (string_of_int c) ^ ")";;
+let rec check p x = match p with 
+	a::b -> if x = a then true else check b x
+	|[] ->  false;;
 	
-print_string (string_of_tree t);;
+let rec sticks p x n = if n<x then  (if (check p n) then "| " ^ (sticks p x (n+2)) else "  " ^ (sticks p x (n+2))) else "";;
+
+let rec string_of_tree l x p = match l with
+	Leaf -> print_string "*"
+	|Node(a,b,c) -> print_string ((string_of_int c) ^ "\n" ^ (sticks p x 0) ^ "+-");
+		if a = Leaf then print_string "*" else string_of_tree a (x+2) (x::p);
+	print_string ("\n" ^ (sticks p x 0) ^ "\\-") ; 
+		if b = Leaf then print_string "*" else string_of_tree b (x+2) p;;
+
+string_of_tree t 0 [];;

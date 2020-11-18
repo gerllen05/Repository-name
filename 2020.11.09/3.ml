57013ea010882cbe40221469faf367f27d8af80f
gerllen05 <49240493+gerllen05@users.noreply.github.com>
2020-11-18 10:19:09 +0300

    Update 3.ml

--- 2020.11.09/3.ml
+++ 2020.11.09/3.ml
@@ -1,6 +1,6 @@
 type tree = Tree of tree list;;
 
-let l = [Tree [Tree [Tree [];Tree[Tree [];Tree[Tree [];Tree[]]]];Tree[Tree [];Tree[Tree [];Tree[]]]];Tree [Tree [Tree [Tree [];Tree[]];Tree[Tree [];Tree[]]];Tree[Tree [];Tree[]]]];;
+let l = [Tree[Tree[];Tree[Tree[Tree[];Tree[]];Tree[]]];Tree[Tree[];Tree[]]];;
 
 let rec check p x = match p with 
 	a::b -> if x = a then true else check b x
@@ -9,11 +9,13 @@ let rec check p x = match p with
 let rec sticks p x n = if n<x then  (if (check p n) then "| " ^ (sticks p x (n+2)) else "  " ^ (sticks p x (n+2))) else "";;
 
 let rec niceprint l x p = match l with
-	a :: b -> print_string ("*\n" ^ (sticks p x 0) ^ "+-");
+	a :: b :: [] -> print_string ("*\n" ^ (sticks p x 0) ^ "+-");
 		(match a with
 		Tree [] -> niceprint [] (x+2) p
 		|Tree c -> niceprint c (x+2) (x::p));
-	print_string ("\n" ^ (sticks p x 0) ^ "\\-") ; niceprint b (x+2) p
+	print_string ("\n" ^ (sticks p x 0) ^ "\\-") ; (match b with
+		Tree [] -> niceprint [] (x+2) p
+		|Tree c -> niceprint c (x+2) p)
 	|[] -> print_string "*";;
 	
 niceprint l 0 [];;
